ca04e83ed9ca03005066a17ea19d3bc27f28c458
WangXiaoDong <514587649@qq.com>
2018-04-19 09:42:07 +0800

    Initial Commit

---
+++ TrainingBlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TrainingBlock
+//
+//  Created by wxd on 2018/4/19.
+//  Copyright © 2018年 wxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TrainingBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TrainingBlock
+//
+//  Created by wxd on 2018/4/19.
+//  Copyright © 2018年 wxd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
